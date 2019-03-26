1705f23addbf025c94e610bf5af3da52a78d5d72
Marina Nicolai <nicolaimarina@gmail.com>
2019-03-26 14:59:03 -0400

    Portofolio styling

--- front-page.php
+++ front-page.php
@@ -18,8 +18,8 @@ get_header();
 
 <nav id="menu">
   <a class="menu-links first hideme" data-menuanchor="firstPage" href="#firstPage">Marina Nicolai</a>
-  <a class="menu-links second" data-menuanchor="secondPage" href="#secondPage">Skills</a>
-  <a class="menu-links third" data-menuanchor="3rdPage" href="#3rdPage">Portofolio</a>
+  <a class="menu-links second" data-menuanchor="secondPage" href="#secondPage">Portofolio</a>
+  <a class="menu-links third" data-menuanchor="3rdPage" href="#3rdPage">Education</a>
   <a class="menu-links fourth" data-menuanchor="4thpage" href="#4thpage">Contact me</a>
 </nav>
 
@@ -35,8 +35,84 @@ get_header();
 	taking bike rides when the weather is nice.
 	</div>
 	<div class="section" id="section1">
-		<div class="slide" id="slide1"><h1>Slide Backgrounds</h1></div>
-		<div class="slide" id="slide2"><h1>Totally customizable</h1></div>
+		<p>
+		Check out some of my projects
+        </p>
+        <div class="items">
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item1.png" alt="" />
+            </div>
+            <div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item2.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item3.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item4.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item5.png" alt="" />
+            </div>
+            <div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item6.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item7.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item8.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-image">
+              <img src="img/items/item9.png" alt="" />
+            </div>
+			<div class="item-decription">
+              <p>in this project I used</p>
+            </div>
+          </div>
+        </div>
+	  </div>
 	</div>
 	<div class="section" id="section2"><h1>Lovely images <br />for a lovely page</h1></div>
 	<div class="section" id="section3"><h1>One Image = One thousand words</h1></div>
