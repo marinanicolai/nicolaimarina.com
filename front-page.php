b7e2185c15b2f5704b3bc7d8a25a35ed6bb5bbe2
Marina Nicolai <nicolaimarina@gmail.com>
2019-03-26 14:19:50 -0400

    Styling nav and first page

--- front-page.php
+++ front-page.php
@@ -15,18 +15,25 @@
  */
 get_header();
 ?>
+
 <nav id="menu">
-  <a class="menu-links hideme" data-menuanchor="firstPage" href="#firstPage">Marina Nicolai</a>
-  <a class="menu-links" data-menuanchor="secondPage" href="#secondPage">section 2</a>
-  <a class="menu-links" data-menuanchor="3rdPage" href="#3rdPage">section 3</a>
-  <a class="menu-links" data-menuanchor="4thpage" href="#4thpage">section 4</a>
+  <a class="menu-links first hideme" data-menuanchor="firstPage" href="#firstPage">Marina Nicolai</a>
+  <a class="menu-links second" data-menuanchor="secondPage" href="#secondPage">Skills</a>
+  <a class="menu-links third" data-menuanchor="3rdPage" href="#3rdPage">Portofolio</a>
+  <a class="menu-links fourth" data-menuanchor="4thpage" href="#4thpage">Contact me</a>
 </nav>
+
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 	<!-- Section A: Specialize -->
   <div id="fullpage">
-	<div class="section " id="section0"><h1>fullPage.js</h1></div>
+	<div class="section " id="section0">
+	<p>I am a web developer who fell in love with programming in my free time.
+	I enjoy learning about new technologies and have passion for web development.
+	If I am not in front of my computer, I am spending time with loved ones, or 
+	taking bike rides when the weather is nice.
+	</div>
 	<div class="section" id="section1">
 		<div class="slide" id="slide1"><h1>Slide Backgrounds</h1></div>
 		<div class="slide" id="slide2"><h1>Totally customizable</h1></div>
